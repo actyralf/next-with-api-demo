933d4d4e0c8a11390bb9236d4ddcee0299935464
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-11-15 16:15:23 +0100

    fetch products in product dashboard

---
+++ pages/api/products/index.js
@@ -0,0 +1,29 @@
+import { products } from "../_data/products";
+
+export default function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      if (req.query.category) {
+        return res
+          .status(200)
+          .json(
+            products.filter(
+              (product) => product.category === req.query.category
+            )
+          )
+          .map((p) => {
+            const { detail, ...rest } = p;
+            return rest;
+          });
+      } else {
+        return res.status(200).json(
+          products.map((p) => {
+            const { detail, ...rest } = p;
+            return rest;
+          })
+        );
+      }
+    default:
+      return res.status(400).json({ error: "method not supported" });
+  }
+}
