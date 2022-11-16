512befa10d7e38cd8242c64b5c508e4f303d9192
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-11-16 23:13:46 +0100

    fix products route

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,32 +1,17 @@
 import { products } from "../_data/products";
 
 export default function handler(req, res) {
-  console.log("req.method", req.method);
-  console.log("req.query", req.query);
-  console.log("req.body", req.body);
-
   switch (req.method) {
     case "GET":
-      if (req.query.category) {
-        return res
-          .status(200)
-          .json(
-            products.filter(
-              (product) => product.category === req.query.category
-            )
-          )
-          .map((p) => {
-            const { detail, ...rest } = p;
-            return rest;
-          });
-      } else {
-        return res.status(200).json(
-          products.map((p) => {
-            const { detail, ...rest } = p;
-            return rest;
-          })
-        );
-      }
+      const filteredProducts = req.query.category
+        ? products.filter((product) => product.category === req.query.category)
+        : products;
+      return res.status(200).json(
+        filteredProducts.map((p) => {
+          const { detail, ...rest } = p;
+          return rest;
+        })
+      );
     default:
       return res.status(400).json({ error: "method not supported" });
   }
